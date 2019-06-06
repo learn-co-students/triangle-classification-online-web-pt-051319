c1aeabc35ee01c7fdb3df169bacb82d768e66cd7
Jess Rezac <jess.rezac@gmail.com>
2019-06-06 21:02:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,42 @@
+require 'pry'
+
 class Triangle
+  attr_reader :side_1, :side_2, :side_3
 
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
-  def initialize(side1, side2, side3)
+  def valid?
+    side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_1 + side_3 > side_2
+  end
 
+  def equilateral?
+    side_1 == side_2 && side_2 == side_3
+  end
+
+  def isosceles?
+    side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+  end
+
+  def scalene?
+    side_1 != side_2 || side_2 != side_3 || side_1 != side_3
   end
 
   def kind
+    unless valid?
+      raise TriangleError
+    end
 
+    if equilateral?
+      :equilateral
+    elsif isosceles?
+      :isosceles
+    elsif scalene?
+      :scalene
+    end
   end
 
   class TriangleError < StandardError
