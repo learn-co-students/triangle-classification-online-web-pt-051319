d4485295646a6ad96918401ae774aec96f5a2fda
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-07 19:36:05 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :first_side, :second_side, :third_side, :equilateral, :isosceles,:scalene
+  @sides = []
+  
+  def initialize(first_side, second_side, third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+    @sides << self 
+  end 
+  
+  def kind 
+    if self.sides.uniq.length == 1
+      :equilateral
+    elsif self.sides.uniq.length == self.sides.length
+     :scalene
+    elsif self.sides.uniq.length == 2 
+     :isosceles 
+   else 
+      raise TriangleError
+    end 
+  end 
+  
+  
+  def sides
+    @sides 
+  end 
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
