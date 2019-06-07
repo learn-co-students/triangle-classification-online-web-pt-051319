b3dc077421f4840799de2faf2397dbd82c9e0802
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-06-07 20:34:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,36 @@
+require 'pry'
+
 class Triangle
   attr_accessor :first_side, :second_side, :third_side, :equilateral, :isosceles,:scalene
-  @sides = []
+  #@@sides =[]
   
   def initialize(first_side, second_side, third_side)
     @first_side = first_side
     @second_side = second_side
     @third_side = third_side
-    @sides << self 
+    @@sides = [first_side, second_side, third_side] 
+  end 
+    
+  def sides
+    @@sides 
   end 
   
   def kind 
+    
     if self.sides.uniq.length == 1
       :equilateral
-    elsif self.sides.uniq.length == self.sides.length
+    elsif self.sides.uniq.length == self.sides.length #&& !self.sides.detect{|num| num.negative?} 
      :scalene
     elsif self.sides.uniq.length == 2 
      :isosceles 
    else 
-      raise TriangleError
-    end 
+     raise TriangleError
+    end  
   end 
   
-  
-  def sides
-    @sides 
-  end 
+
   
   class TriangleError < StandardError
-    # triangle error code
+    #"Your case violates one of the inequality rules for Triangles."
   end
 end
